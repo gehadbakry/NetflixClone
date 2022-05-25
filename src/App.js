9708aa940d30def1c20e9562fe049f51b4f38c9c
aya-mahmoud <aya.mahmoud.lotfy@gmail.com>
2022-05-25 03:40:19 +0200

    Register page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Pages/Home';
+import Register from './Pages/register/Register';
 import Watch from './Pages/watch/Watch';
 
 function App() {
   return (
     <div className="App">
       {/* <Home/> */}
-      <Watch/>
+      {/* <Watch/> */}
+      <Register/>
      </div>
   );
 }
