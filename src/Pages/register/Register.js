43bf81f694ef8ddd0d7afa3fde355917656cf70e
aya-mahmoud <aya.mahmoud.lotfy@gmail.com>
2022-05-26 02:48:05 +0200

    register page UI updates

--- src/Pages/register/Register.js
+++ src/Pages/register/Register.js
@@ -1,6 +1,20 @@
+import { useRef } from 'react';
 import './Register.css';
 
 function Register() {
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+
+    const  emailRef=useRef()
+    const  passwordRef=useRef()
+
+
+    const handleStart=()=>{
+        setEmail(emailRef.current.value)
+    }
+    const handleFinish=()=>{
+        setEmail(passwordRef.current.value)
+    }
     return ( 
         <div className="register">
             <div className="top">
@@ -18,12 +32,20 @@ function Register() {
                 <p>
                 Ready to watch? Enter your email to create or restart your membership.
                 </p>
+                {!email ? (
                 <div className="input">
-                    <input type="email" placeholder="Email address" />
-                    <button className="registerButton">Get Started</button>
+                    <input type="email" placeholder="Email address" ref={emailRef} />
+                    <button className="registerButton" onClick={handleStart}>Get Started</button>
                 </div>
+                ):( <form className="input">
+                         <input type="passsword" placeholder="password" ref={passwordRef} />
+                        <button className="registerButton" onClick={handleFinish}>
+                            Start </button>
+                    </form>)}
             </div>
+            
         </div>
+        
     );
 }
 
