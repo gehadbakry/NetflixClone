f507445a51ba6aa974d043cf45d927792e74f0e2
gehadbakry <gehadk.bakry@gmail.com>
2022-05-30 17:51:44 +0200

    fetchData

--- Front-end/src/Pages/Home.js
+++ Front-end/src/Pages/Home.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
 import './Home.css'
 import NavBar from '../Components/NavBar/NavBar'
 import Featued from '../Components/Featured/Featured'
 import  List from '../Components/List/List'
+import axios from 'axios'
 
 
 const Home =({type})=>{
+
+    const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+  
+    useEffect(() => {
+      const getRandomLists = async () => {
+        try {
+          const res = await axios.get(
+            `lists${type ? "?type=" + type : ""}${
+              genre ? "&genre=" + genre : ""
+            }`,
+            {
+              headers: {
+                token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOTI3ZGM4MmY3YWUwYjJiZmM0MjJhMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MzkwNjk3OSwiZXhwIjoxNjU0MzM4OTc5fQ.Owe1NainJZA9no6RdwwFIqDca9zZ-AevJ3yqLvmZqqg",
+              },
+            }
+          );
+          console.log(res);
+          setLists(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getRandomLists();
+    }, [type, genre]);
 return(
     <div className='home'>
         <NavBar/>
        <Featued type={type}/>
+       {lists.map((list) => (
+        <List list={list} />
+      ))}
+       {/* <List/>
        <List/>
        <List/>
-       <List/>
-       <List/>
+       <List/> */}
 
        {/* <img  src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             /> */}
