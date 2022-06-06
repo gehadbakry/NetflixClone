7d5c23846d1e46ad3f0b25dd0b14705db4b1c227
aya-mahmoud <aya.mahmoud.lotfy@gmail.com>
2022-06-06 19:49:27 +0200

    Fixing Sign in Button

--- Front-end/src/Pages/register/Register.js
+++ Front-end/src/Pages/register/Register.js
@@ -45,11 +45,17 @@ function Register() {
                         
                     />
                  
-                    <button className="loginButton"  ><NavLink to="/login" style={{ color: 'inherit',textDecoration: 'none'  }}> Sign In </NavLink></button>
+                        <NavLink to="/login" >
+                          <button className="loginButton"  >
+                             Sign In 
+                            </button>
+                        </NavLink>
+
+                             
                     
                 </div>
             </div>
-            <div className="Container">
+            <div className="container ">
                 <h1>Unlimited movies, TV shows, and more.</h1>
                 <h2>Watch anywhere. Cancel anytime.</h2>
                 <p>
@@ -59,6 +65,7 @@ function Register() {
                 <div className="input">
                     <input type="email" placeholder="Email address" onChange={(e)=>{setEmail(e.target.value)}} />
                     <button className="registerButton" onClick={handleStart}>Start</button>
+                    
                 </div>
                 ):( <form className="input">
                           <input type="username" placeholder="username" onChange={(e)=>{setUsername(e.target.value)}} />
