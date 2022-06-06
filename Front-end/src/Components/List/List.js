7e321a32996f22ae242b732eb2b08d99ba2e922e
DoniaWaelSamir <doniawaelsamir@gmail.com>
2022-06-06 21:27:53 +0200

    Movie List Updates

--- Front-end/src/Components/List/List.js
+++ Front-end/src/Components/List/List.js
@@ -10,6 +10,7 @@ export default function List({list}){
     const [isMoved,setIsMoved] = useState(false);
 
 
+   // console.log(list.content[0].length)
     // list.content.map((item)=>
     // console.log(item)
     // )
@@ -23,6 +24,7 @@ export default function List({list}){
             listRef.current.style.transform =`translateX(${distance + 230}px)`
         }
 
+        else if(direction === "right" && slideNumber < list.content[0].length - 1){
             setSlideNumber(slideNumber+1);
             listRef.current.style.transform =`translateX(${distance - 230}px)`
         }
@@ -39,6 +41,9 @@ export default function List({list}){
                         {items.map((item, index) => <li key={index}>{item.name}</li>)}
                     </ul> */}
                     {/* Index which is send ti list item */}
+                    {/* {console.log(list.content[0])} */}
+                        {list.content[0].map((item,index)=>(
+                             <ListItem key={index} index={index} item={item}/>
                         ) )}
 
     
