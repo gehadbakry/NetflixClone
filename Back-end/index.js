d30ef6b79dcb3c943da6c9c641af92de97a3c455
aya-mahmoud <aya.mahmoud.lotfy@gmail.com>
2022-06-07 16:58:14 +0200

    index.js

--- Back-end/index.js
+++ Back-end/index.js
@@ -11,36 +11,32 @@ const listRoute = require("./Routes/lists");
 dotenv.config();
 
 //  CHECK YOUR DB CONNECTION    
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-   
-})
-.then(()=>{console.log("Connected success")})
-.catch(err=>console.log(err));
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
- app.use(cors(corsOptions))
-<<<<<<< HEAD
- 
-=======
->>>>>>> parent of e99b8c0 (PaymentyFirstTry)
+    })
+    .then(() => { console.log("Connected success") })
+    .catch(err => console.log(err));
 
- async function main() {
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+async function main() {
     await mongoose.connect(process.env.MONGO_URL);
-  }
+}
 
 app.use(express.json());
 
-app.use("/api/auth", authRoute );
-app.use("/api/movies",movieRoute);
-app.use("/api/users",userRoute);
-app.use("/api/lists",listRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/users", userRoute);
+app.use("/api/lists", listRoute);
 
-app.listen(8800,()=>{
+app.listen(8800, () => {
     console.log("listening to port 8800")
 });
